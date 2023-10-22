c6dc2c67ac8bae72e3a39268d6ae0ac11dd29c29
Matheus Pedroni <pnmatheus@protonmail.com>
2023-10-21 21:29:26 -0300

    quicksort

---
+++ sorting/quicksort.go
@@ -0,0 +1,29 @@
+package main
+
+func QuickSort(arr []int) {
+	quicksort(arr, 0, len(arr)-1)
+}
+
+func quicksort(arr []int, lo int, hi int) {
+	if lo >= hi {
+		return
+	}
+
+	pivotIdx := partition(arr, lo, hi)
+	quicksort(arr, lo, pivotIdx-1)
+	quicksort(arr, pivotIdx+1, hi)
+}
+
+func partition(arr []int, lo int, hi int) int {
+	pivot := arr[hi]
+	idx := lo - 1
+	for i := lo; i < hi; i++ {
+		if arr[i] <= pivot {
+			idx++
+			arr[i], arr[idx] = arr[idx], arr[i]
+		}
+	}
+	idx++
+	arr[idx], arr[hi] = pivot, arr[idx]
+	return idx
+}
